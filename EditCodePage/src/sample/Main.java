93f247abbafda4646b68a495cdf209b56322baba
Luodian <1121058986@qq.com>
2018-01-26 18:15:58 +0800

    添加了富文本编辑器

--- EditCodePage/src/sample/Main.java
+++ EditCodePage/src/sample/Main.java
@@ -5,29 +5,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.fxmisc.richtext.demo.JavaKeywordsAsync;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("EditCodePage.fxml"));
-//        primaryStage.setTitle("Hello World");
-//
-//        Controller mycontroller =
-//        Scene scene = new Scene (root);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception
+    {
 	    FXMLLoader myEditPageLoader = new FXMLLoader (getClass ().getResource ("EditCodePage.fxml"));
 	    Parent root = myEditPageLoader.load();
-	    
 	    Controller EditPageController = myEditPageLoader.getController ();
-	    primaryStage.setTitle ("Hello World");
+	    primaryStage.setTitle ("Patronus");
+	    try {
+		    URL iconURL = Main.class.getResource("resources/patronus.png");
+		    Image image = new ImageIcon (iconURL).getImage();
+		    com.apple.eawt.Application.getApplication().setDockIconImage(image);
+	    } catch (Exception e) {
+		    e.printStackTrace ();
+	    }
 	    Scene scene = new Scene(root);
+	    scene.getStylesheets().add(JavaKeywordsAsync.class.getResource("java-keywords.css").toExternalForm());
 	    primaryStage.setScene (scene);
 	    primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
