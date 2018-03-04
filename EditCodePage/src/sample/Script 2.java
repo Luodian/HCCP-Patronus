91074a31e93a35cd5793b7680a7f49c60d332bfa
Luodian <1121058986@qq.com>
2018-03-04 19:45:03 +0800

    mar-4

---
+++ EditCodePage/src/sample/Script 2.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Script {
+	private String ScriptName;
+	public Script(String _scriptName)
+	{
+		this.ScriptName = _scriptName;
+	}
+	public String getScriptName()
+	{
+		return this.ScriptName;
+	}
+}
