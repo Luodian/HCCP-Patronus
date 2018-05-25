8e9c1217b99ff24af622d3245a576f6ec20ffba6
elrond wang <309426989@qq.com>
2018-05-25 21:44:11 +0800

    May 25,2018 3nd

--- src/sample/Controller/Code/NeuralNetworkGraphController.java
+++ src/sample/Controller/Code/NeuralNetworkGraphController.java
@@ -1,10 +1,13 @@
 package sample.Controller.Code;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.animation.ParallelTransition;
+import javafx.animation.PathTransition;
 import javafx.animation.RotateTransition;
-import javafx.event.ActionEvent;
+import javafx.animation.Timeline;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
@@ -14,19 +17,18 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.CubicCurve;
-import javafx.scene.shape.StrokeLineCap;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import sample.StartProcess;
 import sample.Utils.ColorGerator;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.ResourceBundle;
 
-public class NeuralNetworkGraphController {
+public class NeuralNetworkGraphController implements Initializable {
 
     @FXML
     public JFXButton select_data_btn;
@@ -58,13 +60,18 @@ public class NeuralNetworkGraphController {
     private int inputNum = 5;
     private List<Integer> hiddenNums = new ArrayList<>();       //记录每个隐藏层的神经元个数
 
+    private ParallelTransition parallelTransition = new ParallelTransition();
+    private boolean createLine = true;
+
     private double input_left;
     private double input_top;
     private double input_bottom;
     private double hidden_left;
     private double output_left;
-    private double radius = 20;
+    private double radius = 10;
     private double btn_height = 35;
+
+    private Group rects = new Group();
 
 
     @FXML
@@ -80,23 +87,35 @@ public class NeuralNetworkGraphController {
 
     }
 
-    public void init() {
-        input_left = 100;
-        hidden_left = 100 + 100;
-        input_top = canvas.getLayoutY();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        input_left = 200;
+        hidden_left = 200 + 100;
         input_bottom = canvas.getLayoutY() + canvas.getPrefHeight();
-
-        output_left = 100 + 100 + 280;
-
-        hiddenNums.add(4);
-        hiddenNums.add(5);
+        output_left = 200 + 100 + 280;
+        hiddenNums.add(6);
+        hiddenNums.add(2);
+        hiddenNums.add(6);
         hiddenNums.add(3);
+        hiddenNums.add(5);
+        hiddenNums.add(4);
 
         addChildrenToCanvas();
 
+        createLine = false;
+        parallelTransition.stop();
+        parallelTransition.getChildren().clear();
+        addChildrenToCanvas();
+        System.out.println(parallelTransition.getChildren().size());
+        parallelTransition.setCycleCount(Timeline.INDEFINITE);
+        parallelTransition.play();
+        createLine = true;
     }
 
+
     void addChildrenToCanvas() {
+        rects.getChildren().clear();
+        parallelTransition.getChildren().clear();
 
         getInput();
         getHidden();
@@ -104,7 +123,7 @@ public class NeuralNetworkGraphController {
 
 
         canvas.getChildren().clear();
-        canvas.getChildren().addAll(lines, inputLayer.circleGroup, inputLayer.labelGroup, outputLayer);
+        canvas.getChildren().addAll(lines, rects, inputLayer.circleGroup, inputLayer.labelGroup, outputLayer);
         for (Group group : hiddenLayers.hiddenAddOrSub) {
             canvas.getChildren().add(group);
         }
@@ -112,17 +131,15 @@ public class NeuralNetworkGraphController {
             canvas.getChildren().add(group);
         }
     }
-
     void getInput() {
         int num = inputNum;
         Group circleGroup = new Group();
         double hspace = (hidden_left - input_left - 2 * radius) / 2;
         double vspace = (input_bottom - input_top - num * 2 * radius) / (1 + num);
 
-        String[] inputNames = {"X1", "X2", "X1*X2", "sin(X1)", "sin(X2)"};
+        String[] inputNames = {"X1", "X2", "X3", "sin(X1)", "sin(X2)"};
         for (int i = 0; i < num; i++) {
             Circle circle = new Circle(radius);
-//            circle.setFill(new ImagePattern(new Image(Main.class.getResourceAsStream("I.png"))));
             circle.setLayoutX(input_left + hspace);
             circle.setLayoutY(input_top + (vspace + 2 * radius) * i + vspace + btn_height);
             circleGroup.getChildren().add(circle);
@@ -153,9 +170,9 @@ public class NeuralNetworkGraphController {
             Button addBtn = new Button("+");
             Button subBtn = new Button("-");
             addBtn.setLayoutX(hidden_left + i * (hiddenLayers.hspace + 2 * radius));
-            addBtn.setLayoutY(-18);
+            addBtn.setLayoutY(20);
             subBtn.setLayoutX(hidden_left + i * (hiddenLayers.hspace + 2 * radius) + hiddenLayers.hspace / 2);
-            subBtn.setLayoutY(-18);
+            subBtn.setLayoutY(20);
 
             int finalI = i;
             subBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -166,16 +183,35 @@ public class NeuralNetworkGraphController {
                         return;
                     hiddenNums.set(finalI, hiddenNums.get(finalI) - 1);
                     addChildrenToCanvas();
+
+                    createLine = false;
+                    parallelTransition.stop();
+                    parallelTransition.getChildren().clear();
+                    addChildrenToCanvas();
+                    System.out.println(parallelTransition.getChildren().size());
+                    parallelTransition.setCycleCount(Timeline.INDEFINITE);
+                    parallelTransition.play();
+                    createLine = true;
+
                 }
             });
 
             addBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
-                    if (hiddenNums.get(finalI) == 5)
+                    if (hiddenNums.get(finalI) >= 8)
                         return;
                     hiddenNums.set(finalI, hiddenNums.get(finalI) + 1);
                     addChildrenToCanvas();
+
+                    createLine = false;
+                    parallelTransition.stop();
+                    parallelTransition.getChildren().clear();
+                    addChildrenToCanvas();
+                    System.out.println(parallelTransition.getChildren().size());
+                    parallelTransition.setCycleCount(Timeline.INDEFINITE);
+                    parallelTransition.play();
+                    createLine = true;
                 }
             });
 
@@ -244,8 +280,8 @@ public class NeuralNetworkGraphController {
     void getOutput() {
         Image image = new Image(String.valueOf(getClass().getResource("../../resources/MDicon/result.png")));
         outputLayer = new ImageView(image);
-        outputLayer.setLayoutX(output_left + 280);
-        outputLayer.setLayoutY(0);
+        outputLayer.setLayoutX(output_left + 200);
+        outputLayer.setLayoutY(canvas.getPrefHeight() / 2 - 50);
         outputLayer.setFitWidth(100);
         outputLayer.setFitHeight(100);
         if (hiddenNums.isEmpty()) {
@@ -256,9 +292,12 @@ public class NeuralNetworkGraphController {
     }
 
     void addLineBtwLayerAndOutput(Group group1) {
+
+        String curColor = ColorGerator.getRandomColor();
         for (int i = 0; i < group1.getChildren().size(); i++) {
             Circle inputCircle = (Circle) group1.getChildren().get(i);
             CubicCurve cubicCurve = new CubicCurve();
+            cubicCurve.getStrokeDashArray().addAll(30d, 20d);
             cubicCurve.setStartX(inputCircle.getLayoutX());
             cubicCurve.setStartY(inputCircle.getLayoutY());
             cubicCurve.setControlX1((inputCircle.getLayoutX() + outputLayer.getLayoutX()) * 5 / 11);
@@ -268,61 +307,88 @@ public class NeuralNetworkGraphController {
             cubicCurve.setEndX(outputLayer.getLayoutX() + outputLayer.getFitWidth() / 2);
             cubicCurve.setEndY(outputLayer.getLayoutY() + outputLayer.getFitHeight() / 2);
 
-            cubicCurve.setStroke(Color.valueOf(ColorGerator.getRandomColor()));
+            cubicCurve.setStroke(Color.valueOf(curColor));
             cubicCurve.setStrokeWidth(2);
             cubicCurve.setStrokeLineCap(StrokeLineCap.ROUND);
             cubicCurve.setFill(Color.CORNSILK.deriveColor(0, 0, 1, 0));
-            lines.getChildren().add(cubicCurve);
+            if (createLine)
+                lines.getChildren().add(cubicCurve);
+            double len = Math.sqrt(Math.pow(cubicCurve.getEndY() - cubicCurve.getStartY(), 2) + Math.pow(cubicCurve.getEndX() - cubicCurve.getStartX(), 2));
+            //len¬‘–°”⁄œﬂµƒ◊‹≥§∂»
+            double rectWidth = 20;
+            for (int j = 0; j < len / (0.9 * rectWidth); j++) {
+                Rectangle rectangle = new Rectangle(inputCircle.getLayoutX(), inputCircle.getLayoutY(), rectWidth, 2.4);
+                rectangle.setArcHeight(0.5);
+                rectangle.setArcWidth(0.5);
+                rectangle.setFill(Color.valueOf(curColor));
+                Path path = new Path();
+                path.getElements().add(new MoveTo(cubicCurve.getStartX(), cubicCurve.getStartY()));
+                path.getElements().add(new CubicCurveTo(cubicCurve.getControlX1(), cubicCurve.getControlY1(), cubicCurve.getControlX2(), cubicCurve.getControlY2(), cubicCurve.getEndX(), cubicCurve.getEndY()));
+                PathTransition pathTransition = new PathTransition();
+                pathTransition.setDuration(Duration.millis(5000));
+                pathTransition.setPath(path);
+                pathTransition.setNode(rectangle);
+                pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+                pathTransition.setCycleCount(Timeline.INDEFINITE);
+                pathTransition.setAutoReverse(false);
+                pathTransition.setDelay(Duration.millis(700 * j));
+                rects.getChildren().add(rectangle);
+                parallelTransition.getChildren().add(pathTransition);
+            }
         }
     }
 
-    /**
-     * 在两层之间添加贝塞尔曲线
-     **/
+    //‘⁄¡Ω≤„÷Æº‰ÃÌº”±¥»˚∂˚«˙œﬂ
     void addLineBtw2Layer(Group group1, Group group2) {
+        String curColor = ColorGerator.getRandomColor();
         for (int i = 0; i < group1.getChildren().size(); i++) {
             Circle inputCircle = (Circle) group1.getChildren().get(i);
             for (int j = 0; j < group2.getChildren().size(); j++) {
                 Circle hiddenCircle = (Circle) group2.getChildren().get(j);
                 CubicCurve cubicCurve = new CubicCurve();
+                cubicCurve.getStrokeDashArray().addAll(25d, 10d);
                 cubicCurve.setStartX(inputCircle.getLayoutX());
                 cubicCurve.setStartY(inputCircle.getLayoutY());
                 cubicCurve.setControlX1((inputCircle.getLayoutX() + hiddenCircle.getLayoutX()) * 7 / 15);
                 cubicCurve.setControlY1((inputCircle.getLayoutY() + hiddenCircle.getLayoutY()) * 7 / 15);
                 cubicCurve.setControlX2((inputCircle.getLayoutX() + hiddenCircle.getLayoutX()) * 8 / 15);
                 cubicCurve.setControlY2((inputCircle.getLayoutY() + hiddenCircle.getLayoutY()) * 8 / 15);
+//                cubicCurve.setControlX1(inputCircle.getLayoutX());
+//                cubicCurve.setControlY1(inputCircle.getLayoutY());
+//                cubicCurve.setControlX2(inputCircle.getLayoutX());
+//                cubicCurve.setControlY2(inputCircle.getLayoutY());
                 cubicCurve.setEndX(hiddenCircle.getLayoutX());
                 cubicCurve.setEndY(hiddenCircle.getLayoutY());
 
-                cubicCurve.setStroke(Color.valueOf(ColorGerator.getRandomColor()));
-                cubicCurve.setStrokeWidth(2);
+                cubicCurve.setStroke(Color.valueOf(curColor));
+                cubicCurve.setStrokeWidth(1);
                 cubicCurve.setStrokeLineCap(StrokeLineCap.ROUND);
                 cubicCurve.setFill(Color.CORNSILK.deriveColor(0, 0, 1, 0));
-                lines.getChildren().add(cubicCurve);
+                if (createLine)
+                    lines.getChildren().add(cubicCurve);
+                double len = Math.sqrt(Math.pow(cubicCurve.getEndY() - cubicCurve.getStartY(), 2) + Math.pow(cubicCurve.getEndX() - cubicCurve.getStartX(), 2));
+                double rectWidth = 15;
+                for (int k = 0; k < len / (1.2 * rectWidth); k++) {
+                    Rectangle rectangle = new Rectangle(inputCircle.getLayoutX(), inputCircle.getLayoutY(), rectWidth, 2.4);
+                    rectangle.setArcHeight(0.5);
+                    rectangle.setArcWidth(0.5);
+                    rectangle.setFill(Color.valueOf(curColor));
+                    Path path = new Path();
+                    path.getElements().add(new MoveTo(cubicCurve.getStartX(), cubicCurve.getStartY()));
+                    path.getElements().add(new CubicCurveTo(cubicCurve.getControlX1(), cubicCurve.getControlY1(), cubicCurve.getControlX2(), cubicCurve.getControlY2(), cubicCurve.getEndX(), cubicCurve.getEndY()));
+                    PathTransition pathTransition = new PathTransition();
+                    pathTransition.setDuration(Duration.millis(3500));
+                    pathTransition.setPath(path);
+                    pathTransition.setNode(rectangle);
+                    pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+                    pathTransition.setCycleCount(Timeline.INDEFINITE);
+                    pathTransition.setAutoReverse(false);
+                    pathTransition.setDelay(Duration.millis(k * 440));
+                    rects.getChildren().add(rectangle);
+                    parallelTransition.getChildren().add(pathTransition);
+                }
             }
         }
-    }
-
-    @FXML
-    public void select_btn(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void subLayer(ActionEvent actionEvent) {
-        if (hiddenNums.size() == 0)
-            return;
-        hiddenNums.remove(hiddenNums.size() - 1);
-
-        addChildrenToCanvas();
-    }
-
-    @FXML
-    public void addLayer(ActionEvent actionEvent) {
-        if (hiddenNums.size() * 2 * radius > output_left - hidden_left)
-            return;
-        hiddenNums.add(new Random().nextInt(4) + 1);
-
-        addChildrenToCanvas();
     }
 
 }
